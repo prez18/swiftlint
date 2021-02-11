30a0cd5b37ec61bf2757e5889aff68366738cd7e
prez18 <yasuhiro.hashimoto.gssi@gmail.com>
2021-02-11 10:46:04 +0900

    ファーストコミット

---
+++ SwiftLintSample/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftLintSample
+//
+//  Created by prez on 2021/02/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let vc = "hige";
+        _ = vc
+    }
+
+    func voidReturn() -> Void {
+        print("void return")
+    }
+
+}
+
